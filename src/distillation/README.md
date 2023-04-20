9f9d6aace655092c5c50e5a832376af84c07a1fe
Vít Tlustoš <tlustos.vit@gmail.com>
2023-04-20 15:35:38 +0200

    Updated train script.

--- src/distillation/README.md
+++ src/distillation/README.md
@@ -1,8 +1,12 @@
 singularity exec --nv /storage/brno12-cerit/home/xtlust05/images/pytorch-ffmpeg-opencv_gcc.SIF bash
 python preprocess.py -d common_voice -o /storage/brno12-cerit/home/xtlust05/datasets/common_voice/
 
-export PYTHONPATH=$PYTHONPATH:.
+
 python3.8 src/distillation/train.py -d dataset/ -t ./runs/
+
+singularity exec --nv /storage/brno12-cerit/home/xtlust05/images/pytorch-ffmpeg-opencv_gcc.SIF bash
+export PYTHONPATH=$PYTHONPATH:.
+python src/distillation/train.py -d ../../datasets/common_voice/ -o runs/ -t distill 
 
 CUDA_VISIBLE_DEVICES=0,1,2,3
 python -m torch.distributed.launch --nproc_per_node 4 src/distillation/train.py -d dataset/ -t ./runs/
--- src/distillation/train.py
+++ src/distillation/train.py
@@ -9,39 +9,40 @@ from transformers import (Seq2SeqTrainer, Seq2SeqTrainingArguments,
 from huggingface_hub.hf_api import HfFolder 
 HfFolder.save_token('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')
 
-def train(dataset_path, train_dir_path, language=("cs", "Czech")):
+def train(data_dir, out_dir, batch_size, trainer, language="czech"):
 
     # setup data pipeline
     pipeline_name = "openai/whisper-large-v2"
     processor = WhisperProcessor.from_pretrained(
-        pipeline_name, language=language[1], task="transcribe")
+        pipeline_name, language=language, task="transcribe")
 
     # setup dataset
-    common_voice = load_from_disk(dataset_path)
+    common_voice = load_from_disk(data_dir)
     data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
 
     # initialize student and teacher models
     student_model = WhisperForConditionalGeneration \
         .from_pretrained("openai/whisper-small")
     student_model.config.forced_decoder_ids = processor \
-        .get_decoder_prompt_ids(language=language[1].lower(), task="transcribe")
+        .get_decoder_prompt_ids(language=language, task="transcribe")
     student_model.config.suppress_tokens = []
 
-    teacher_model = WhisperForConditionalGeneration \
-        .from_pretrained("openai/whisper-large-v2")
-    teacher_model.config.forced_decoder_ids = processor \
-        .get_decoder_prompt_ids(language=language[1].lower(), task="transcribe")
-    teacher_model.config.suppress_tokens = []
-    teacher_model.to('cuda:0').half()
+    if trainer == "distill":
+        teacher_model = WhisperForConditionalGeneration \
+            .from_pretrained("openai/whisper-large-v2")
+        teacher_model.config.forced_decoder_ids = processor \
+            .get_decoder_prompt_ids(language=language, task="transcribe")
+        teacher_model.config.suppress_tokens = []
+        teacher_model.to('cuda:0').half()
 
     # setup training process
     training_args = Seq2SeqTrainingArguments(
-        output_dir=train_dir_path,
-        per_device_train_batch_size=16,
+        output_dir=out_dir,
+        per_device_train_batch_size=batch_size,
         gradient_accumulation_steps=1,
         learning_rate=1e-5,
         warmup_steps=500,
-        max_steps=4000,
+        max_steps=5000,
         gradient_checkpointing=True,
         fp16=True,
         evaluation_strategy="steps",
@@ -59,7 +60,7 @@ def train(dataset_path, train_dir_path, language=("cs", "Czech")):
         logging_first_step=True
     )
 
-    if False:
+    if trainer == "seq2seq":
         trainer = Seq2SeqTrainer(
             args=training_args,
             model=student_model,
@@ -69,19 +70,19 @@ def train(dataset_path, train_dir_path, language=("cs", "Czech")):
             compute_metrics=WER(tokenizer=processor.tokenizer),
             tokenizer=processor.feature_extractor,
         )
-    
-    trainer = DistillationTrainer(
-        config=training_args,
-        student_model=student_model,
-        teacher_model=teacher_model,
-        train_dataset=common_voice["train"],
-        eval_dataset=common_voice["test"],
-        tokenizer=processor.tokenizer,
-        data_collator=data_collator,
-        compute_metrics=WER(tokenizer=processor.tokenizer),
-        temperature=2.0,
-        supervised=False
-    )
+    elif trainer == "distill":
+        trainer = DistillationTrainer(
+            config=training_args,
+            student_model=student_model,
+            teacher_model=teacher_model,
+            train_dataset=common_voice["train"],
+            eval_dataset=common_voice["test"],
+            tokenizer=processor.tokenizer,
+            data_collator=data_collator,
+            compute_metrics=WER(tokenizer=processor.tokenizer),
+            temperature=2.0,
+            supervised=False
+        )
 
     # evaluate model before the first step
     class EvaluateFirstStepCallback(TrainerCallback):
@@ -94,12 +95,19 @@ def train(dataset_path, train_dir_path, language=("cs", "Czech")):
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("-d", "--dataset", dest="dataset_path",
-                      help="Path to the dataset")
-    parser.add_option("-t", "--train-dir", dest="train_dir_path",
-                      help="Path to the training directory")
+    parser.add_option("-d", "--data-dir", dest="data_dir",
+                      help="Path to the dataset directory.")
+    parser.add_option("-o", "--out-dir", dest="out_dir",
+                      help="Path to the output directory.")
+    parser.add_option("-b", "--batch-size", dest="batch_size",
+                      help="Batch size.", default=32)
+    parser.add_option("-t", "--trainer", dest="trainer",
+                      help="Trainer (either seq2seq or distill)", default=("distill"))
     parser.add_option("-l", "--language", dest="language",
-                      help="Language to use (default: cs,Czech)", default=("cs", "Czech"))
+                      help="Language to use (default: cs,Czech)", default="czech")
+  
     (options, args) = parser.parse_args()
 
-    train(options.dataset_path, options.train_dir_path, options.language)
+    train(options.data_dir, options.out_dir, options.batch_size,
+        options.trainer, options.language
+    )
