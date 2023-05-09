57c0f43128346233ca8e8ae617611b815de7d193
Vít Tlustoš <tlustos.vit@gmail.com>
2023-05-09 08:34:09 +0200

    Error handling

--- src/train.py
+++ src/train.py
@@ -55,15 +55,15 @@ def train(out_dir,
         for param in student_model.parameters():
             param.requires_grad = False  # freeze the model - train adapters later
             if param.ndim == 1:
-                # cast the small parameters (e.g. layernorm) to fp32 for stability
                 param.data = param.data.to(torch.float32)
 
-            student_model.gradient_checkpointing_enable()  # reduce number of stored activations
-            student_model.enable_input_require_grads()
-            class CastOutputToFloat(torch.nn.Sequential):
-                def forward(self, x): 
-                    return super().forward(x).to(torch.float32)
-                student_model.lm_head = CastOutputToFloat(student_model.lm_head)
+        student_model.gradient_checkpointing_enable()
+        student_model.enable_input_require_grads()
+
+        class CastOutputToFloat(torch.nn.Sequential):
+            def forward(self, x): 
+                return super().forward(x).to(torch.float32)
+        student_model.lm_head = CastOutputToFloat(student_model.lm_head)
 
     if lora:
         config = LoraConfig(
